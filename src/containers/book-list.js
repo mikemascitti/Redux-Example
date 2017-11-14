d3776a17afd7e519f7321c83099852904103265b
MASCITTI, MICHAEL <michaelmascitti@northwesternmutual.com>
2017-11-14 15:59:10 -0600

    completed section 4

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-
-export default class BookList extends Component {
+class BookList extends Component {
   renderList(){
-    return this.props.books.map((book) => {
-      reutrn(
-        <li key={book.title} className="list-group-item" > {book.title}</li>
+    return this.props.books.map((bookObj) => {
+      return(
+        <li key={bookObj.title} className="list-group-item" > {bookObj.title} </li>
+
       );
 
 
@@ -13,14 +14,25 @@ export default class BookList extends Component {
   }
 
   render() {
-    return {
+    return (
       <ul className="list-group col-sm-4">
         {this.renderList()}
       </ul>
-    }
+    )
   }
+}
 
+function mapStateToProps(state) {
+  //Whatever is returned will show up as props
+  // inside of book list.
+  return {
+    //the connect function causes this object to be a property of the Component
 
-
+    books: state.listOfBooks  //state.listOfBooks was created in the index.js from the data in the reducer.
+    //the reducer_books applied the prop of title to each item in this state.
+  };
 
 }
+//Why don't we have to pass in a parameter into mapStateToProps?  it takes one!
+
+export default connect(mapStateToProps)(BookList);

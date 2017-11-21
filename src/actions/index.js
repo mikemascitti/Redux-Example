5f984b50895ca25003bdd88330eda99960fa75f8
Mike Mascitti <33425796+mikemascitti@users.noreply.github.com>
2017-11-21 16:40:37 -0600

    Action Created

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,14 @@
 
 export function selectBook(aBook) {
   //export the function so we can use it
-  console.log('A book ', aBook.title);
+  //console.log('A book ', aBook.title);
 
-  
+
+  //selectBook is an ActionCreator, it needs to return an action,
+  // an object with a type property.
+  return {
+    type: 'BOOK_SELECTED' ,
+    payload: book
+  };
+
 }

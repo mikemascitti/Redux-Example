831eee017398b86a504307e83594bc1a94c93c11
MASCITTI, MICHAEL <michaelmascitti@northwesternmutual.com>
2017-11-22 15:24:49 -0600

    Completed Redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,14 +2,7 @@
 
 export function selectBook(aBook) {
   //export the function so we can use it
-  //console.log('A book ', aBook.title);
-
-
-  //selectBook is an ActionCreator, it needs to return an action,
-  // an object with a type property.
-  return {
-    type: 'BOOK_SELECTED' ,
-    payload: aBook
-  };
-
+  console.log('A book ', aBook.title);
 }
+
+
